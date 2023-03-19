d467e25a1da681cdf6000ff0456844636badbac1
Sena <36813961+helenaferdy@users.noreply.github.com>
2023-03-19 13:31:04 +0700

    init push

---
+++ main.py
@@ -0,0 +1,33 @@
+from turtle import Screen
+from time import sleep
+from turtlehelena import Helenaturtle, Helenawall, Helenatext
+
+NUMBER_OF_TURTLE = 9
+X_POS = -400
+Y_POS = [-300, -225, -150, -75, 0, 75, 150, 225, 300]
+COLORS = ["red", "blue", "green", "purple", "brown", "gray", "black", "yellow", "pink",]
+
+
+game_is_on = True
+screen = Screen()
+
+thewall = Helenawall()
+theturtle = Helenaturtle("turtle", COLORS, X_POS, Y_POS, NUMBER_OF_TURTLE)
+
+text = Screen()
+guess = text.textinput("Guess the winner", "Guess turtle color to win the race:")
+
+message = ""
+while game_is_on:
+    xcolor = theturtle.move_turtle()
+    if xcolor in COLORS:
+        game_is_on = False
+        if guess.lower() == xcolor:
+            message = (f"You Won!\nThe {xcolor} turtle won")
+        else:
+            message = (f"You Lost!\nThe {xcolor} turtle won")
+
+thetext = Helenatext(message)
+
+
+screen.exitonclick()
---
+++ turtlehelena.py
@@ -0,0 +1,52 @@
+from turtle import Turtle
+import random
+
+class Helenaturtle:
+    def __init__(self, shape, color, x, y, number):
+        self.shape = shape
+        self.color = color
+        self.x = x
+        self.y = y
+        self.number = number
+
+        self.turtles= []
+        self.new_turtle()
+
+    def new_turtle(self):
+        for i in range(self.number):
+            t = Turtle()
+            t.penup()
+            t.shape(self.shape)
+            t.color(self.color[i])
+            t.goto(self.x, self.y[i])
+
+            self.turtles.append(t)
+
+    def move_turtle(self):
+        self.xcors = []
+        for t in self.turtles:
+            if t.xcor() <380:
+                t.forward(random.randint(0,30))
+            else:
+                return t.fillcolor()
+            
+
+    
+class Helenawall:
+    def __init__(self):
+        self.w = Turtle()
+        self.w.penup()
+        self.w.shape("square")
+        self.w.color("black")
+        self.w.shapesize(400,1)
+        self.w.goto(405,0)
+
+class Helenatext:
+    def __init__(self, message):
+        self.message = message
+
+        self.t = Turtle()
+        self.t.hideturtle()
+        self.t.penup()
+        self.t.goto(-100, 0)
+        self.t.write(self.message, font=("Arial", 20, "normal"))
